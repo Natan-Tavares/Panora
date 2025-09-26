c7d6494a837348367c725993250bf6cdad261d91
Davilapeixoto <davilapeixoto52@gmail.com>
2025-09-26 14:01:55 -0300

    Inicio do BD e adicionado pasta para templates no settings

--- JCPE/AppJCPE/admin.py
+++ JCPE/AppJCPE/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Noticia, Resposta
+
+admin.site.register(Noticia)
+admin.site.register(Resposta)
--- JCPE/AppJCPE/models.py
+++ JCPE/AppJCPE/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+import datetime
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+class Noticia(models.Model):
+    titulo = models.CharField(max_length=200, null=False)
+    materia = models.TextField(null=False)
+    data_criacao = models.DateTimeField("Criado em ", auto_now_add=True)
+    autor = models.CharField(max_length=200, null=False)
+    
+    def __str__(self):
+        return "[" + str(self.id) + "] " + self.titulo
+
+class Resposta(models.Model):
+    noticia = models.ForeignKey(Noticia, on_delete=models.CASCADE)
+    texto = models.TextField(null=False)
+    data_criacao = models.DateTimeField("Criado em ", auto_now_add=True)
+    usuario = models.CharField(max_length=200, null=False)
+    
+    def __str__(self):
+        return "[" + str(self.id) + "] " + self.texto
+    
