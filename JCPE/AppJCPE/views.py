52dba8bafea51761302637269a29010aa7f77c99
Davilapeixoto <davilapeixoto52@gmail.com>
2025-10-01 18:01:53 -0300

    criado a pagina de criação e visualização de noticias

--- JCPE/AppJCPE/views.py
+++ JCPE/AppJCPE/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
-
+from .models import Noticia, Resposta
+from django.contrib.auth.models import User
+from django.utils.timezone import now
 # Create your views here.
+
+def criar_noticia(request):
+    if request.method == "POST":
+        titulo=request.POST.get("titulo")
+        materia=request.POST.get("materia")
+        autor=request.user
+        data=now()
+        Noticia.objects.create(titulo=titulo,materia=materia,autor=autor,data_criacao=data)
+    
+    return render(request,'criar_noticia.html')
+
+def inicial(request):
+    noticias=Noticia.objects.all()
+    return render(request,'inicial.html', {'noticias':noticias})
+
--- JCPE/JCPE/urls.py
+++ JCPE/JCPE/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from AppJCPE import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('criar/', views.criar_noticia),
+    path('', views.inicial),
 ]
