1669872ea2718c08d4803daa74432a33c16326ce
guilhermeblacerda <guilacerda1822@gmail.com>
2025-10-22 13:54:34 -0300

    adiconar link do deploy

--- JCPE/JCPE/settings.py
+++ JCPE/JCPE/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-6ldn5ux9p_&oa=1y1-wdjt(a=hou+_pa5ck8=z1^9&kl!-%k0@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
